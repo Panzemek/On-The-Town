a2eb86ac9b401af2454519d078b88d7cdd4e1b7d
Stacy Nowak <nowak.stacy@gmail.com>
2019-03-17 17:02:40 -0700

    arrays to add to dropbox menus and functions to pick restaruants based on parameter and to randomly pick one and post to html - just set to body for now

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,17 +1,44 @@
-$.ajax({  
-    url: "https://developers.zomato.com/api/v2.1/search?q=pizza&cities?id=279",
-    dataType: 'json',
-    method: "GET",
-    beforeSend: function(xhr){xhr.setRequestHeader('user-key', 
-    'c676a8f84163c31b3e525853910c4a76')}
-}).then(function(response){
-    console.log(response);
-});
+let cuisineArr = ["American", "Italian", "Chinese", "Japanese", "Thai", "Vietnamese", "Mediterranean", "Mexican", "African", "Indian"]
+let neighborhoodArr = ["Downtown", "University District", "Ballard", "Belltown", "Central District", "Fremont", "Greenwood/Phinney", "Magnolia", "Columbia City", "Broadview/Bitter Lake", "Interbay", "Beacon Hill", "Sodo", "Maple Leaf", "Ravenna", "Capitol Hill", "West Seattle", "International District", "Wallingford", "Georgetown", "Lake City", "Northgate", "South Lake Union", "Queen Anne: Lower", "White Center", "Queen Anne: Upper", "Rainier Valley", "Madison Park", "Green Lake", "Madrona/Leschi"]
 
-$.ajax({  
-    url: "https://www.eventbriteapi.com/v3/events/search/?location.address=seattle&location.within=10km&expand=venue&token=QHBNEFWIRBGDKAUY44N7",
-    dataType: 'json',
-    method: "GET",
-}).then(function(response){
-    console.log(response);
-});
+function randomRestaurantPick(response) {
+    let arr = response.restaurants;
+    let randomPick = arr[Math.floor(Math.random() * arr.length)];
+
+    let rpName = randomPick.restaurant.name;
+    let rpLink = randomPick.restaurant.url;
+    let rpCuisine = randomPick.restaurant.cuisines;
+    let rpLocation = randomPick.restaurant.location.locality_verbose;
+
+    console.log (rpName, rpCuisine, rpLocation, rpLink);
+    nameLink = $("<a href="+rpLink+" target=_blank>"+rpName+"</a>");
+    rpImg = $("<img src="+randomPick.restaurant.thumb+">")
+
+    $("body").append("<p><a href="+rpLink+" target=_blank>"+rpName+"</a></p>");
+    $("body").append("<p>"+rpCuisine+"</p>");
+    $("body").append("<p>"+rpLocation+"</p>");
+    $("body").append(rpImg);
+}
+
+function pullRestaurantInfo(param) {
+
+    let QueryUrl = "https://developers.zomato.com/api/v2.1/search?q=" + param + "&cities?q=seattle";
+
+    $.ajax({  
+        url: QueryUrl,
+        dataType: 'json',
+        method: "GET",
+        beforeSend: function(xhr){xhr.setRequestHeader('user-key', 
+        'c676a8f84163c31b3e525853910c4a76')}
+    }).then(function(response){
+        console.log(response);
+        randomRestaurantPick(response);
+    });
+}
+
+// random seattle restaurant
+pullRestaurantInfo("restaurant");
+// cuisine search
+pullRestaurantInfo("Mediterranean");
+// neighborhood search
+pullRestaurantInfo("Wallingford");
