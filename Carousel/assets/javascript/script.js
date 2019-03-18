0cb7d7b4aed46a287fddc760cbfafae76236d201
kth <katiehaster@gmail.com>
2019-03-18 11:07:13 -0700

    bootstrap carousel, both carousels respond to goButton

--- Carousel/assets/javascript/script.js
+++ Carousel/assets/javascript/script.js
@@ -1,87 +1,14 @@
 $(document).ready(function(){
-
-    
-    // function createSlick(){
-    
-        console.log("hello");
-
-        // $('.goButton').on('click', function() {
-        //     slick.slickNext();
-        // });
-
-        $('.slick-next').on('click', function () {
-            slick.slickNext();
-        });
-
-        $('#eventWheel').not('.slick-initialized').slick({
-            infinite: true,
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            settings: {
-                arrows: true,
-            }
-            
-          });
-
-});
+    $("#goButton").on("click", function() {
+        $("#eventCarousel").carousel("cycle");
+        console.log("clickCarousel");
+        $("#restaurantCarousel").carousel("cycle");
+        console.log("clickRestaurant");
        
-        // $('.eventWheel').slick({
-        //     centerMode: true,
-        //     centerPadding: 0,
-        //     slidesToShow: 3,
-            
-        //     responsive: [
-        //     {
-        //         breakpoint: 768,
-        //     settings: {
-        //         arrows: false,
-        //     centerMode: true,
-        //     centerPadding: '5px',
-        //     slidesToShow: 3
-        //     }
-        //     },
-        //     {
-        //         breakpoint: 480,
-        //     settings: {
-        //         arrows: false,
-        //     centerMode: true,
-        //     centerPadding: '5px',
-        //     slidesToShow: 1
-        //     }
-        //     }
-        //     ]
-        // })
-
-        // $('.restaurantWheel').slick({
-        //     centerMode: true,
-        //     centerPadding: '5px',
-        //     slidesToShow: 3,
-            
-        //     responsive: [
-        //     {
-        //         breakpoint: 768,
-        //     settings: {
-        //         arrows: false,
-        //     centerMode: true,
-        //     centerPadding: '5px',
-        //     slidesToShow: 3
-        //     }
-        //     },
-        //     {
-        //         breakpoint: 480,
-        //     settings: {
-        //         arrows: false,
-        //     centerMode: true,
-        //     centerPadding: '5px',
-        //     slidesToShow: 1
-        //     }
-        //     }
-        //     ]
-        // })
-
-      
-    // }
-    // createSlick();  
+    });
+    
+});
+ 
 
         
 
