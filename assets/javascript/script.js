238294f97a3c936d1831cf1107d34da0a9d7f2f1
Stacy Nowak <nowak.stacy@gmail.com>
2019-03-18 14:26:49 -0700

    just a branch to play with code, no important things here

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,9 +9,29 @@ $.ajax({
 });
 
 $.ajax({  
-    url: "https://www.eventbriteapi.com/v3/events/search/?location.address=seattle&location.within=10km&expand=venue&token=QHBNEFWIRBGDKAUY44N7",
+    url: "https://www.eventbriteapi.com/v3/events/search/?token=QHBNEFWIRBGDKAUY44N7&categories=103&location.address=seattle",
     dataType: 'json',
     method: "GET",
 }).then(function(response){
     console.log(response);
 });
+
+
+
+function geocodeRestaurantSearch(lat, lon) {
+
+    let QueryUrl = "https://developers.zomato.com/api/v2.1/search?q=restaurant&locations?query=seattle&lat="+lat+"&lon="+lon;
+
+    $.ajax({  
+        url: QueryUrl,
+        dataType: 'json',
+        method: "GET",
+        beforeSend: function(xhr){xhr.setRequestHeader('user-key', 
+        'c676a8f84163c31b3e525853910c4a76')}
+    }).then(function(response){
+        console.log(response);
+        randomRestaurantPick(response);
+    });
+}
+
+geocodeRestaurantSearch(47.690755, -122.356263);
