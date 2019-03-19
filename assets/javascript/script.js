8017aa1149c4ea795d760047ac6df21ba3e4e6d4
Stacy Nowak <nowak.stacy@gmail.com>
2019-03-19 12:45:26 -0700

    using google places app to find nearby restaurants

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,36 +1,37 @@
-// Zomato API key: c676a8f84163c31b3e525853910c4a76
-// Eventbrite API key: QHBNEFWIRBGDKAUY44N7
-// Google API key: AIzaSyDF_fqwmBu3FLIxPBFJLXZuWD5l-23ts74
-// Yelp API key: W8IP0_T7u9NtFkNEdcsD26F6YvMBIpTdxAGPPwJWuAIUGEa764q5SBRbwPDJB2ORpQfoJEtKQp2H_K4gU7AgtaQjyi20F1eIYtNcYXav7Cyk55kiKyUE4JUqGGuQXHYx
 
 
-// initial seach success with zomato api
-$.ajax({  
-    url: "https://developers.zomato.com/api/v2.1/search?q=pizza&cities?id=279",
-    dataType: 'json',
-    method: "GET",
-    beforeSend: function(xhr){xhr.setRequestHeader('user-key', 
-    'c676a8f84163c31b3e525853910c4a76')}
-}).then(function(response){
-    console.log("initial zomato search: ", response);
-});
+function googleNearbyRestaurants(lat,lon,cuisine) {
+
+    queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+lat+","+lon+"&radius=2000&type=restaurant&keyword="+cuisine+"&key=AIzaSyDF_fqwmBu3FLIxPBFJLXZuWD5l-23ts74"
+
+    $.ajax({  
+        url: queryURL,
+        dataType: 'json',
+        method: "GET",
+    }).then(function(response){
+        console.log("google places nearby search: ", response);
+        randomRestaurantPick(response);
+    });
+    
+}
+
+function randomRestaurantPick(response) {
+    let arr = response.results;
+    let randomPick = arr[Math.floor(Math.random() * arr.length)];
+    console.log("Random Selection from array: ", randomPick);
+
+    let restaurant = results.name;
+    let restLatitude = results.geometry.location[lat];
+    let restLongitude = results.geometry.location[lng];
+
+    pullRestaurantInfo(restaurant,restLatitude,restLongitude)
+}
 
 
-// initial search success with eventbrite api
-$.ajax({  
-    url: "https://www.eventbriteapi.com/v3/events/search/?token=QHBNEFWIRBGDKAUY44N7&location.address=seattle&expand=venue",
-    dataType: 'json',
-    method: "GET",
-}).then(function(response){
-    console.log("initial eventbrite search: ", response);
-});
 
+function pullRestaurantInfo(name,lat,lng) {
 
-
-// searching restaurants by latitude & longitude- radius does not work
-function geocodeRestaurantSearch(lat, lon) {
-
-    let QueryUrl = "https://developers.zomato.com/api/v2.1/search?q=restaurant&locations?query=seattle&lat="+lat+"&lon="+lon+"&radius=500";
+    let QueryUrl = "https://developers.zomato.com/api/v2.1/search?q="+name+"&locations?query=seattle&lat="+lat+"&lon="+lng;
 
     $.ajax({  
         url: QueryUrl,
@@ -39,43 +40,29 @@ function geocodeRestaurantSearch(lat, lon) {
         beforeSend: function(xhr){xhr.setRequestHeader('user-key', 
         'c676a8f84163c31b3e525853910c4a76')}
     }).then(function(response){
-        console.log("Zomato lat&lon restaurant search: ", response);
-    });
-}
-// hard coded as taproot theater in greenwood for testing
-geocodeRestaurantSearch(47.690755, -122.356263);
-
-
-// Geocode search url that returns neighborhood by address search:
-// https://maps.googleapis.com/maps/api/geocode/json?address=1136+south+albro+place,+seattle,+WA&key=AIzaSyDF_fqwmBu3FLIxPBFJLXZuWD5l-23ts74
-
-
-function playingWithYelp() {
-
-    let QueryUrl = "https://api.yelp.com/v3/businesses/search?term=delis&latitude=37.786882&longitude=-122.399972";
-
-    $.ajax({  
-        url: QueryUrl,
-        dataType: 'json',
-        method: "GET",
-        headers: {
-            'Authorization':'Bearer W8IP0_T7u9NtFkNEdcsD26F6YvMBIpTdxAGPPwJWuAIUGEa764q5SBRbwPDJB2ORpQfoJEtKQp2H_K4gU7AgtaQjyi20F1eIYtNcYXav7Cyk55kiKyUE4JUqGGuQXHYx',
-        },    
-    }).then(function(response){
-        console.log("Yelp response: ", response);
+        console.log(response);
+        populateRestaurantInfo(response);
     });
 }
 
-playingWithYelp();
+function populateRestaurantInfo(response) {
+    let item = response.restaurants;
+
+    let rpName = item.restaurant.name;
+    let rpLink = item.restaurant.url;
+    let rpCuisine = item.restaurant.cuisines;
+    let rpLocation = item.restaurant.location.locality_verbose;
+
+    console.log (rpName, rpCuisine, rpLocation, rpLink);
+    nameLink = $("<a href="+rpLink+" target=_blank>"+rpName+"</a>");
+    rpImg = $("<img src="+randomPick.restaurant.thumb+">")
+
+    $("body").append("<p><a href="+rpLink+" target=_blank>"+rpName+"</a></p>");
+    $("body").append("<p>"+rpCuisine+"</p>");
+    $("body").append("<p>"+rpLocation+"</p>");
+    $("body").append(rpImg);
+}
 
 
-// multiple parameters
-$.ajax({  
-    url: "https://developers.zomato.com/api/v2.1/search?q=mexican+italian&cities?id=279",
-    dataType: 'json',
-    method: "GET",
-    beforeSend: function(xhr){xhr.setRequestHeader('user-key', 
-    'c676a8f84163c31b3e525853910c4a76')}
-}).then(function(response){
-    console.log("Mulitple parameters: ", response);
-});
+
+googleNearbyRestaurants(47.6138108,-122.3196641,"mexican");
