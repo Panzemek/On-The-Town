e17a0cc6ced3dfd9dc744bda08a21ed55141ecba
Stacy Nowak <nowak.stacy@gmail.com>
2019-03-19 14:40:52 -0700

    functions for google api pulls

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,17 @@
+function randomSeattleRestaurants() {
+
+    queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+seattle&key=AIzaSyDF_fqwmBu3FLIxPBFJLXZuWD5l-23ts74"
+
+    $.ajax({  
+        url: queryURL,
+        dataType: 'json',
+        method: "GET",
+    }).then(function(response){
+        console.log("seattle restaurant search: ", response);
+        randomRestaurantPick(response);
+    });
+    
+}
 
 
 function googleNearbyRestaurants(lat,lon,cuisine) {
@@ -20,49 +34,45 @@ function randomRestaurantPick(response) {
     let randomPick = arr[Math.floor(Math.random() * arr.length)];
     console.log("Random Selection from array: ", randomPick);
 
-    let restaurant = results.name;
-    let restLatitude = results.geometry.location[lat];
-    let restLongitude = results.geometry.location[lng];
+    let restID = randomPick.place_id;
 
-    pullRestaurantInfo(restaurant,restLatitude,restLongitude)
+    console.log("Place ID: ", restID)
+
+    pullRestaurantInfo(restID)
 }
 
+function pullRestaurantInfo(restID) {
 
-
-function pullRestaurantInfo(name,lat,lng) {
-
-    let QueryUrl = "https://developers.zomato.com/api/v2.1/search?q="+name+"&locations?query=seattle&lat="+lat+"&lon="+lng;
+    let QueryUrl = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/details/json?placeid="+restID+"&key=AIzaSyDF_fqwmBu3FLIxPBFJLXZuWD5l-23ts74";
 
     $.ajax({  
         url: QueryUrl,
         dataType: 'json',
         method: "GET",
-        beforeSend: function(xhr){xhr.setRequestHeader('user-key', 
-        'c676a8f84163c31b3e525853910c4a76')}
     }).then(function(response){
-        console.log(response);
+        console.log("place detailed restaurant info: ", response);
         populateRestaurantInfo(response);
     });
 }
 
+
 function populateRestaurantInfo(response) {
-    let item = response.restaurants;
-
-    let rpName = item.restaurant.name;
-    let rpLink = item.restaurant.url;
-    let rpCuisine = item.restaurant.cuisines;
-    let rpLocation = item.restaurant.location.locality_verbose;
-
-    console.log (rpName, rpCuisine, rpLocation, rpLink);
-    nameLink = $("<a href="+rpLink+" target=_blank>"+rpName+"</a>");
-    rpImg = $("<img src="+randomPick.restaurant.thumb+">")
+    let item = response.result;
+    let rpName = item.name;
+    let rpLink = item.website;
+    let rpLocation = item.address_components[2].long_name;
+    let rpImgRef = item.photos[0].photo_reference;
+    console.log("Photo ref: ", rpImgRef);
+    
+    let imgLink = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference="+rpImgRef+"&key=AIzaSyDF_fqwmBu3FLIxPBFJLXZuWD5l-23ts74";
+    
 
     $("body").append("<p><a href="+rpLink+" target=_blank>"+rpName+"</a></p>");
-    $("body").append("<p>"+rpCuisine+"</p>");
-    $("body").append("<p>"+rpLocation+"</p>");
-    $("body").append(rpImg);
+    $("body").append("<p>Location: "+rpLocation+"</p>");
+    $("body").append("<img src="+imgLink+">");
+
 }
 
 
-
-googleNearbyRestaurants(47.6138108,-122.3196641,"mexican");
+randomSeattleRestaurants();
+googleNearbyRestaurants(47.6138108,-122.3196641,"italian");
