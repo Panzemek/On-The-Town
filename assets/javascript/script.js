cdaf1500d24c6c163654b8133a095ea933ba4ff0
kth <katiehaster@gmail.com>
2019-03-20 13:43:35 -0700

    granim background

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,10 +3,10 @@ var granimInstance = new Granim({
     element: '#canvas-image-blending',
     direction: 'top-bottom',
     isPausedWhenNotInView: true,
-    image: {
-        source: 'https://cdn.pixabay.com/photo/2018/01/31/05/30/panoramic-3120304_1280.jpg',
-        blendingMode: 'multiply'
-    },
+    // image: {
+    //     source: 'https://cdn.pixabay.com/photo/2018/01/31/05/30/panoramic-3120304_1280.jpg',
+    //     blendingMode: 'multiply'
+    // },
     states: {
         "default-state": {
             gradients: [
@@ -15,7 +15,7 @@ var granimInstance = new Granim({
                 ['#4B3D60', '#152852'],
                 ['#73434B', '#40284A']
             ],
-            transitionSpeed: 7000
+            transitionSpeed: 3000
         }
     }
 });
